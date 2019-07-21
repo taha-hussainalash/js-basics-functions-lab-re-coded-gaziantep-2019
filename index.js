c26424bd9735372e6be9cb494f336b3c90f7a09d
Taha HussainAlash <taha.benzma94@gmail.com>
2019-07-21 03:25:53 +0000

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(xBlocks) {
+  return Math.abs(xBlocks - 42);
+}
+
+function distanceFromHqInFeet(xBlocks){
+  return distanceFromHqInBlocks(xBlocks) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400 ){
+    return 0;
+  }
+  else if ( distance > 400 && distance <= 2000 ){
+    return (distance - 400)  *0.02;
+  }
+  else if (distance < 2500){
+    return 25;
+  }
+  else {
+    return "cannot travel that far";
+  }
+} 
